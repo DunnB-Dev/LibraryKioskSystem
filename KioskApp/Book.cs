f518c15208e3321f7295cc29e59a3aca4bab876b
bryce <bryce@DESKTOP-GGL78P8>
2022-11-20 23:00:16 -0500

    Add project files.

---
+++ KioskApp/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskApp
+{
+    public class Book:IComparable<Book>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Pages { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int CompareTo(Book? other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+        }
+
+        //Print() that displays title, author, pages, and publisher
+
+        public void Print()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Pages: {Pages}, Publisher: {Publisher}");
+        }
+
+    }
+}
